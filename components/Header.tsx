96def051848d70dab8fd32351ebfed1036784b29
fellgar246 <fellgar246@gmail.com>
2023-01-25 13:33:30 -0600

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,43 @@
+import { BellIcon } from '@heroicons/react/24/outline';
+import Link  from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+        <div className="flex items-center space-x-2 md:space-x-10">
+            <img
+            src="https://rb.gy/ulxxee"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+            />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink">Home</li>
+                <li className="headerLink">TV Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My List</li>
+            </ul>
+        </div>
+
+        <div className='flex items-center space-x-4 text-sm font-light'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="hiiden sm:inline w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+            <p className="hidden lg:inline">Kids</p>
+            <BellIcon className='w-6 h-6' />
+
+            <Link href="/account">
+                <img
+                    src="https://rb.gy/g1pwyx"
+                    alt=""
+                    className="cursor-pointer rounded"
+                />
+            </Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
